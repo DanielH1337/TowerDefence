0abbd968a144cd3f51834429d58e3b544030124e
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2023-08-02 22:33:38 +0300

    update

--- TowerDaddy/Assets/Scripts/bullet.cs
+++ TowerDaddy/Assets/Scripts/bullet.cs
@@ -7,12 +7,11 @@ public class bullet : MonoBehaviour
     public Rigidbody rb;
     public Transform enemy;
     public GameObject radar;
-    public float speed = 10f;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        radar.GetComponentInChildren<enemyDetect>();
         enemy.position = Vector3.forward;
        
     }
@@ -20,9 +19,10 @@ public class bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //enemy.position = radar.GetComponentInChildren<enemyDetect>().enemyTransform;
-        rb.transform.Translate(enemy.transform.position*speed);
-        Debug.Log(enemy.transform.position);
+     
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, enemy.position, step);
+       // Debug.Log(enemy.transform.position);
     }
     private void OnCollisionEnter(Collision collision)
     {
