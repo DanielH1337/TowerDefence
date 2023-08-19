86ee9a590b930a84bb7230cf06c2e2ebeabe3b93
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2023-08-19 15:36:03 +0300

    Slidepanel bug fixes

--- TowerDaddy/Assets/Scripts/GameManager.cs
+++ TowerDaddy/Assets/Scripts/GameManager.cs
@@ -6,25 +6,92 @@ using UnityEngine.EventSystems;
 public class GameManager : MonoBehaviour
 {
     int UILayer;
+    public GameObject SlidePanelGO;
     public Animator SlidePanel;
- 
+    private bool sliderCheck;
+    private float slidePanelPosition;
+    //private float slidePanelPosition;
+    private List<bool> SlidePanelCheckList = new List<bool>();
 
     private void Start()
     {
+        
+        slidePanelPosition = SlidePanelGO.transform.position.x;
+        Debug.Log(slidePanelPosition);
         UILayer = LayerMask.NameToLayer("UI");
+        sliderCheck =true;
+        StartCoroutine(PanelSlider());
+        SlidePanelCheckList.Add(false);
+        SlidePanelCheckList.Add(true);
     }
 
     private void Update()
     {
-        print(IsPointerOverUIElement() ? "Over UI" : "Not over UI");
-        if (IsPointerOverUIElement())
+
+        // print(IsPointerOverUIElement() ? "Over UI" : "Not over UI");
+        if (Input.GetKeyDown("space"))
         {
-            if (SlidePanel.isActiveAndEnabled) SlidePanel.SetTrigger("Back");
+            for (int i = 0; i < SlidePanelCheckList.Count; i++)
+            {
+                Debug.Log(SlidePanelCheckList[i]);
+            }
         }
-        if(!IsPointerOverUIElement())
+    }
+    public void FirstPanelSlide()
+    {
+        StartCoroutine(SlidePanelDelayAway());
+    }
+
+     
+    IEnumerator PanelSlider()
+    {
+        while (true)
         {
-            if (SlidePanel.isActiveAndEnabled) SlidePanel.SetTrigger("Away");
+            yield return new WaitForSeconds(1.1f);
+            if (sliderCheck)
+            {
+                SlidePanelCheckList.Insert(0, IsPointerOverUIElement());
+                if (IsPointerOverUIElement() && SlidePanelCheckList[0] != SlidePanelCheckList[1])
+                {
+                    sliderCheck = false;
+                    StartCoroutine(SlidePanelDelayBack());
+
+                }
+                if(!IsPointerOverUIElement() && SlidePanelCheckList[0] != SlidePanelCheckList[1])
+                {
+                    sliderCheck = false;
+                    StartCoroutine(SlidePanelDelayAway());
+
+                }
+
+            }
+            if(SlidePanelCheckList.Count > 2)
+            {
+                SlidePanelCheckList.RemoveAt(2);
+            }
         }
+        
+    }
+    IEnumerator SlidePanelDelayBack()
+    {
+       // Debug.Log("Back");
+       // SlidePanelGO.layer = 0;
+        if (SlidePanel.isActiveAndEnabled) SlidePanel.SetTrigger("Back");
+        yield return new WaitForSeconds(1f);
+        sliderCheck = true;
+      //  SlidePanelGO.layer = 5;
+     
+        
+    }
+    IEnumerator SlidePanelDelayAway()
+    {
+       // Debug.Log("Away");
+      //  SlidePanelGO.layer = 0;
+        if (SlidePanel.isActiveAndEnabled) SlidePanel.SetTrigger("Away");
+        yield return new WaitForSeconds(1f);
+        sliderCheck = true;
+      //  SlidePanelGO.layer = 5;
+
     }
 
 
